692e7378c53cf91445bf20c9deb79721390a2231
Arms <armandcarraz@gmail.com>
2023-10-13 14:40:40 +0200

    Register form updated

--- src/main/java/com/holpapp/RegisterForm.java
+++ src/main/java/com/holpapp/RegisterForm.java
@@ -1,5 +1,42 @@
 package com.holpapp;
+import javax.swing.*;
 
-public class RegisterForm {
-    
+public class RegisterForm extends JFrame {
+    public RegisterForm() {
+        setTitle("Register Form");
+        setSize(300, 250); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        add(panel);
+
+        JLabel userLabel = new JLabel("Username:");
+        JTextField userText = new JTextField(20);
+        panel.add(userLabel);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordText = new JPasswordField(20);
+        panel.add(passwordLabel);
+        panel.add(passwordText);
+
+        JLabel confirmPasswordLabel = new JLabel("Confirm Password:");
+        JPasswordField confirmPasswordText = new JPasswordField(20);
+        panel.add(confirmPasswordLabel);
+        panel.add(confirmPasswordText);
+
+        JLabel emailLabel = new JLabel("Email:");
+        JTextField emailText = new JTextField(20);
+        panel.add(emailLabel);
+        panel.add(emailText);
+
+        JButton registerButton = new JButton("Register");
+        panel.add(registerButton);
+    }
+
+    public static void main(String[] args) {
+        RegisterForm registerForm = new RegisterForm();
+        registerForm.setVisible(true);
+    }
 }
