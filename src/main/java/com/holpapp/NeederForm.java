8ed883f8f48fdbd153c70c869ffd7b3a4593df93
Arms <armandcarraz@gmail.com>
2023-10-16 15:03:24 +0200

    V12

--- src/main/java/com/holpapp/NeederForm.java
+++ src/main/java/com/holpapp/NeederForm.java
@@ -1,5 +1,17 @@
 package com.holpapp;
 
-public class NeederForm {
-    
+import javax.swing.*;
+
+public class NeederForm extends JFrame {
+    public NeederForm() {
+        JLabel label = new JLabel("Hello World!");
+        add(label);
+        pack();
+
+    }
+
+    public static void main(String[] args) {
+        NeederForm neederForm = new NeederForm();
+        neederForm.setVisible(true);
+    }
 }
--- src/main/java/com/holpapp/ValidatingUserForm.java
+++ src/main/java/com/holpapp/ValidatingUserForm.java
@@ -1,5 +1,16 @@
 package com.holpapp;
 
-public class ValidatingUserForm {
-    
+import javax.swing.*;
+
+public class ValidatingUserForm extends JFrame {
+    public ValidatingUserForm() {
+        JLabel label = new JLabel("Hello World!");
+        add(label);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        ValidatingUserForm validatingUserForm = new ValidatingUserForm();
+        validatingUserForm.setVisible(true);
+    }
 }
