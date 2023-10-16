cdffa494d1282c6972050f5afade97070d55da08
leonie <chevalaz@insa-toulouse.fr>
2023-10-16 14:46:59 +0200

    Test

--- src/main/java/com/holpapp/ValidatingUserFrom.java
+++ src/main/java/com/holpapp/ValidatingUserFrom.java
@@ -1,5 +1,16 @@
 package com.holpapp;
 
-public class ValidatingUserFrom {
-    
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ValidatingUserFrom extends JFrame{
+        public ValidatingUserFrom() {
+        setTitle("Helper Form");
+        setSize(250, 750); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        add(panel);
+    }
 }
