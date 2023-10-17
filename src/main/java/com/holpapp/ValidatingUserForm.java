91d7a767502b33a69c700539eadc9d3d3e756b2e
leonie <chevalaz@insa-toulouse.fr>
2023-10-17 16:37:13 +0200

    changes

--- src/main/java/com/holpapp/ValidatingUserForm.java
+++ src/main/java/com/holpapp/ValidatingUserForm.java
@@ -1,19 +1,5 @@
 package com.holpapp;
 
-<<<<<<< HEAD
-import javax.swing.*;
-
-public class ValidatingUserForm extends JFrame {
-    public ValidatingUserForm() {
-        JLabel label = new JLabel("Hello World!");
-        add(label);
-        pack();
-    }
-
-    public static void main(String[] args) {
-        ValidatingUserForm validatingUserForm = new ValidatingUserForm();
-        validatingUserForm.setVisible(true);
-=======
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -26,6 +12,5 @@ public class ValidatingUserForm extends JFrame {
 
         JPanel panel = new JPanel();
         add(panel);
->>>>>>> 614fece085940c88984f51d268e07890f95dae00
     }
 }
